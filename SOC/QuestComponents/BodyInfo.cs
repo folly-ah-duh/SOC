f1c129e0dd02ffa9b3dd9c56baa03751998909d1
Morbidslinky <joe.h.zoeller@gmail.com>
2017-11-13 20:03:45 -0600

    Populated Body List

--- SOC/QuestComponents/BodyInfo.cs
+++ SOC/QuestComponents/BodyInfo.cs
@@ -9,6 +9,48 @@ namespace SOC.QuestComponents
     public static class BodyInfo
     {
         public static BodyInfoEntry[] BodyInfoArray = {
+             new BodyInfoEntry(
+                "AFGH_HOSTAGE",
+                "TppDefine.QUEST_BODY_ID_LIST.AFGH_HOSTAGE_MALE",
+                "/Assets/tpp/parts/chara/prs/prs2_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/prs2_main0_mdl.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "AFGH_HOSTAGE_FEMALE",
+                "TppDefine.QUEST_BODY_ID_LIST.AFGH_HOSTAGE_FEMALE",
+                "/Assets/tpp/parts/chara/prs/prs3_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/prs3_main0_mdl.fpk",
+                true
+                ),
+            new BodyInfoEntry(
+                "MAFR_HOSTAGE",
+                "TppDefine.QUEST_BODY_ID_LIST.MAFR_HOSTAGE_MALE",
+                "/Assets/tpp/parts/chara/prs/prs5_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/ih/prs5_main0_mdl.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "MAFR_HOSTAGE_FEMALE",
+                "TppDefine.QUEST_BODY_ID_LIST.MAFR_HOSTAGE_FEMALE",
+                "/Assets/tpp/parts/chara/prs/prs6_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/prs6_main0_mdl.fpk",
+                true
+                ),
+            new BodyInfoEntry(
+                "DDS_RESEARCHER",
+                "TppDefine.QUEST_BODY_ID_LIST.AFGH_HOSTAGE_MALE",
+                "/Assets/tpp/parts/chara/dds/ddr0_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/ddr0_main0_mdl.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "DDS_RESEARCHER_FEMALE",
+                "TppDefine.QUEST_BODY_ID_LIST.AFGH_HOSTAGE_FEMALE",
+                "/Assets/tpp/parts/chara/dds/ddr1_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/ddr1_main0_mdl.fpk",
+                true
+                ),
             new BodyInfoEntry(
                 "DRAB", 
                 "TppEnemyBodyId.dds3_main0_v00", 
@@ -39,31 +81,143 @@ namespace SOC.QuestComponents
                 ),
             new BodyInfoEntry(
                 "SNEAKING_SUIT",
-                "TppEnemyBodyId.dds3_main0_v00",
-                "/Assets/tpp/parts/chara/dds/dds3_main0_def_v00.parts",
-                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_wait.fpk",
+                "TppEnemyBodyId.dds4_enem0_def",
+                "/Assets/tpp/parts/chara/sna/sna4_enem0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_sneak.fpk",
                 false
                 ),
             new BodyInfoEntry(
                 "SNEAKING_SUIT_FEMALE",
-                "TppEnemyBodyId.dds3_main0_v00",
-                "/Assets/tpp/parts/chara/dds/dds3_main0_def_v00.parts",
-                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_wait.fpk",
-                false
+                "TppEnemyBodyId.dds4_enef0_def",
+                "/Assets/tpp/parts/chara/sna/sna4_enef0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_sneak.fpk",
+                true
                 ),
             new BodyInfoEntry(
                 "BATTLE_DRESS",
-                "TppEnemyBodyId.dds3_main0_v00",
-                "/Assets/tpp/parts/chara/dds/dds3_main0_def_v00.parts",
-                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_wait.fpk",
+                "TppEnemyBodyId.dds5_enem0_def",
+                "/Assets/tpp/parts/chara/sna/sna5_enem0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_btdrs.fpk",
                 false
                 ),
             new BodyInfoEntry(
                 "BATTLE_DRESS_FEMALE",
-                "TppEnemyBodyId.dds3_main0_v00",
-                "/Assets/tpp/parts/chara/dds/dds3_main0_def_v00.parts",
-                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_wait.fpk",
+                "TppEnemyBodyId.dds5_enem0_def",
+                "/Assets/tpp/parts/chara/sna/sna5_enef0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_btdrs.fpk",
+                true
+                ),
+            new BodyInfoEntry(
+                "XOF_GASMASK",
+                "TppEnemyBodyId.wss0_main0_v00",
+                "/Assets/tpp/parts/chara/wss/wss0_main0_def_v00_ih_sol.parts",
+                "/Assets/tpp/pack/mission2/ih/wss0_main0_mdl.fpk",
                 false
+                ),
+            new BodyInfoEntry(
+                "XOF",
+                "TppEnemyBodyId.wss4_main0_v00",
+                "/Assets/tpp/parts/chara/wss/wss4_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_xof_soldier.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "MSF_TPP",
+                "TppEnemyBodyId.dds0_main1_v00",
+                "/Assets/tpp/parts/chara/dds/dds0_main1_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/dds0_main1_mdl.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "DDS_PILOT2",
+                "TppEnemyBodyId.dds5_enem0_def",
+                "/Assets/tpp/parts/chara/dds/dds9_main0_def_v00_ih_hos.parts",
+                "/Assets/tpp/pack/mission2/ih/dds9_main0_mdl.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "WANDERING_MSF",
+                "TppEnemyBodyId.pfs0_dds0_v00",
+                "/Assets/tpp/parts/chara/svs/svs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_afgh.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SOVIET_BERET",
+                "TppEnemyBodyId.svs0_unq_v010",
+                "/Assets/tpp/parts/chara/svs/svs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_afgh.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SOVIET_HOODIE",
+                "TppEnemyBodyId.svs0_unq_v060",
+                "/Assets/tpp/parts/chara/svs/svs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_afgh.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SOVIET_SOLDIER",
+                "TppEnemyBodyId.svs0_snp_v00_b",
+                "/Assets/tpp/parts/chara/svs/svs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_afgh.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "PF_C_BERET",
+                "TppEnemyBodyId.pfs0_unq_v450",
+                "/Assets/tpp/parts/chara/pfs/pfs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_mafr.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "PF_SOLDIER",
+                "TppEnemyBodyId.pfs0_snp_v00_a",
+                "/Assets/tpp/parts/chara/pfs/pfs0_main0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_mafr.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_GW",
+                "TppEnemyBodyId.dlf_enem0_def",
+                "/Assets/tpp/parts/chara/dlf/dlf1_enem0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_GW_FEMALE",
+                "TppEnemyBodyId.dlf_enef0_def",
+                "/Assets/tpp/parts/chara/dlf/dlf0_enem0_def_f_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit.fpk",
+                true
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_GOB",
+                "TppEnemyBodyId.dlg_enem0_def",
+                "/Assets/tpp/parts/chara/dlg/dlg1_enem0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit2.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_GOB_FEMALE",
+                "TppEnemyBodyId.dlg_enef0_def",
+                "/Assets/tpp/parts/chara/dlg/dlg0_enem0_def_f_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit2.fpk",
+                true
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_MEG",
+                "TppEnemyBodyId.dlh_enem0_def",
+                "/Assets/tpp/parts/chara/dlh/dlh1_enem0_def_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit3.fpk",
+                false
+                ),
+            new BodyInfoEntry(
+                "SWIMWEAR_MEG_FEMALE",
+                "TppEnemyBodyId.dlh_enef0_def",
+                "/Assets/tpp/parts/chara/dlh/dlh0_enem0_def_f_v00.parts",
+                "/Assets/tpp/pack/mission2/common/mis_com_dd_soldier_swim_suit3.fpk",
+                true
                 ),
         };
         
